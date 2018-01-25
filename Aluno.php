0dd622398762b1ac70b440eb1587feccfa9c6af2
php 10 <php.10@salsicha.cec.ime.usp.br>
2018-01-24 22:39:24 -0200

    anterior 1 + atual

--- Aluno.php
+++ Aluno.php
@@ -1,17 +1,20 @@
 <?php
-
+use Carbon\Carbon;
 class Aluno
 {
-  public $nome;
-  public $dataDeNascimento;
+  public $nome; // atributo de objeto
+  public $dataDeNascimento; // atributo de objeto
   public $rg;
   public $turma;
+  //atributo de classe
+  public static $contador = 0;
 
   // construtor
   function __construct(string $nome, Carbon $dataDeNascimento)
   {
     $this->nome = $nome;
     $this->dataDeNascimento = $dataDeNascimento;
-    echo "Aluno criado" . PHP_EOL;
+    self::$contador++;
+    echo "Contador: " . Aluno::$contador . PHP_EOL;
   }
 }
